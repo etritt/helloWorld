0c1afa3148c9cf8afe669818b9564444df31a1a9
etritt <tritt.evan@gmail.com>
2013-06-26 11:18:04 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/* HelloWorld.java */
+
+/* Created by Evan Tritt on 6/26/2013 */
+
+public class HelloWorld {
+  public static void main(String[] args)	{
+		 
+		System.out.println("Hello World!");
+	}
+}
